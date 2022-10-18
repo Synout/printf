d6e5a690d12cc2a7cd747a2741df1279b68e7b01
sethgreen23 <dridi.chaith@gmail.com>
2022-10-18 12:47:55 -0700

    first task

--- main.h
+++ main.h
@@ -4,9 +4,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
-
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
 
 
 #endif /* MAIN_H */
